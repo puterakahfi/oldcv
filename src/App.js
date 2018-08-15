84fc9a8e0adddd3d101fd22cae91e2a35f9a4386
puterakahfi <puterakahfi@gmail.com>
2018-08-15 20:41:45 +0700

    add font awesome, bootstap and axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from "@fortawesome/free-brands-svg-icons";
+import axios from 'axios';
 
 class App extends Component {
   render() {
@@ -8,11 +12,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">CV</h1>
+          <h1 className="App-title">Putera Kahfi</h1>
+          <h3>Web Developer</h3>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div class="container">
+        <h3> <FontAwesomeIcon icon={Icons.faGithub} /> Github Activities</h3>
+
+        </div>
       </div>
     );
   }
