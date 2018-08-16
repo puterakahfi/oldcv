5e1c4c803c45fad690ded051519098730a5aa670
puterakahfi <puterakahfi@gmail.com>
2018-08-16 07:22:53 +0700

    add icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import vue from './vuejs.png';
+import sass from './sass.png';
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as Icons from "@fortawesome/free-brands-svg-icons";
-import axios from 'axios';
+import * as Brand from "@fortawesome/free-brands-svg-icons";
+
 
 class App extends Component {
   render() {
@@ -12,16 +14,45 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={vue} className="App-logo no-animation" alt="logo" />
+          <img src={sass} className="App-logo no-animation" alt="logo" />
+
           <h1 className="App-title">Putera Kahfi</h1>
-          <h3>Web Developer</h3>
+          <h2>Web Developer</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <div class="container">
-        <h3> <FontAwesomeIcon icon={Icons.faGithub} /> Github Activities</h3>
 
+        <div className="container">
+          <h3> <FontAwesomeIcon icon={Brand.faGit} /> Skillset</h3>
+
+        </div>
+        <div className="container">
+          <h3> <FontAwesomeIcon icon={Brand.faGithub} /> Github Activities</h3>
+
+        </div>
+
+        <div className="container">
+          <h3> <FontAwesomeIcon icon={Brand.faGit} /> Projects</h3>
+
+        </div>
+
+          <div className="container">
+          <h3> <FontAwesomeIcon icon={Brand.faGit} /> Coding Activities</h3>
+
+        </div>
+
+        <div className="container">
+          <footer>
+            <ul>
+              <li><FontAwesomeIcon icon={Brand.faGithub} /> github</li>
+              <li><FontAwesomeIcon icon={Brand.faLinkedin} /> linkedin</li>
+              <li><FontAwesomeIcon icon={Brand.faFacebook} /> facebook</li>
+
+            </ul>
+          </footer>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Brand from "@fortawesome/free-brands-svg-icons";
+import axios from 'axios';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
