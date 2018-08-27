f0759f5de1bf667223015794542e4f518a8a487a
puterakahfi <puterakahfi@gmail.com>
2018-08-27 18:57:29 +0700

    update coding activity, add by language section

--- src/molecules/CodingSnapshot.js
+++ src/molecules/CodingSnapshot.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
+
 import './styles/CodingSnapshot.css';
 
 class CodingSnapshot extends Component{
@@ -6,8 +9,21 @@ class CodingSnapshot extends Component{
     constructor(props) {
         super(props);
     
-        this.state = {wakatime: 'https://wakatime.com/share/@puterakahfi/4850fd01-3b5e-4922-b350-101d7b6378d1.svg'};
+        this.state = {
+            activeTab: '1',
+            wakatime: 'https://wakatime.com/share/@puterakahfi/4850fd01-3b5e-4922-b350-101d7b6378d1.svg',
+            wakatime2: 'https://wakatime.com/share/@puterakahfi/f4eada33-25e6-4b04-b23c-9e3e7577c7b7.svg',      
+        };
       }
+
+    
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
 
     render()
     {
@@ -18,7 +34,48 @@ class CodingSnapshot extends Component{
                      <span>{this.props.title}</span>
                 </h2> 
                 <hr/>
-                <figure><embed src={this.state.wakatime}></embed></figure>
+
+                <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => { this.toggle('1'); }}
+            >
+              Activity
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2' })}
+              onClick={() => { this.toggle('2'); }}
+            >
+              Language
+            </NavLink>
+          </NavItem>
+        </Nav>
+
+         <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12" >
+              <figure className="coding-item"><embed src={this.state.wakatime}></embed></figure>
+
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+              <figure className="coding-item"><embed src={this.state.wakatime2}></embed></figure>
+
+            </Col>
+            </Row>
+          </TabPane>
+
+        </TabContent>
+
+        
+
             </div>
         );
     }
