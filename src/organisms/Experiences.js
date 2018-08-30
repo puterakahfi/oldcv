a8aec88a249a14e03109fe58d43ebb46b20d10d7
puterakahfi <puterakahfi@gmail.com>
2018-08-30 17:34:35 +0700

    add experiencess sass

---
+++ src/organisms/Experiences.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './styles/Experiences.css'
+
+class Experiences extends Component
+{
+    render(){
+        return(
+            <div className="section">
+            <hr class="section-hr" />
+            <h2>Experiences</h2>
+            </div>
+        )
+    }
+}
+
+export default Experiences
