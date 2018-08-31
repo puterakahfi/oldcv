63584e13baad23c3fde2fc5cf4b3ae5e25ff02ed
puterakahfi <puterakahfi@gmail.com>
2018-08-31 16:31:53 +0700

    refactoring component link to social links

---
+++ src/molecules/SocialLinks.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+
+import './styles/SocialLinks.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import * as Brand from "@fortawesome/free-brands-svg-icons"
+
+
+class SocialLinks extends Component{
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+
+            loading: false,
+            links:[
+                {icon:Brand.faFacebook, link:'https://www.facebook.com/codejunior'},
+                {icon:Brand.faLinkedin, link:'https://www.linkedin.com/in/putera-kahfi-52663b48'},
+                {icon:Brand.faGithubSquare, link:'https://github.com/puterakahfi/'}
+            ],   
+        };
+      }
+  
+
+    render()
+    {
+        return(
+            <div id="social-links">
+            <ul>
+            {this.state.links.map(function(item){
+                return <li>
+                    <a href={item.link} target="_blank"><FontAwesomeIcon size="2x" icon={item.icon} /></a>
+                </li>
+            })
+            }
+        </ul>
+            </div>
+
+        )
+    }
+}
+
+export default SocialLinks
