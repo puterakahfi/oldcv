766b9e3388c50c6e1e7ec8812f6e4d37c9705500
puterakahfi <puterakahfi@gmail.com>
2018-09-01 21:24:05 +0700

    refactoring TechnologyStack Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-import App from './pages/App';
+import Layout from './layout/Layout';
 
 import registerServiceWorker from './registerServiceWorker';
+import {theme} from "./profile.json";
+
+import App from './themes/default/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
