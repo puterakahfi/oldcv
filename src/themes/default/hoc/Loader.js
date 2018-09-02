14cdb709329a89794ff999072187dde162e403b4
puterakahfi <puterakahfi@gmail.com>
2018-09-02 13:07:05 +0700

    add hoc

---
+++ src/themes/default/hoc/Loader.js
@@ -0,0 +1,17 @@
+import React, {
+    Component
+} from 'react';
+
+const Loader = (WrappedComponent) => {
+
+    return class Loader extends Component {
+
+        render(){
+
+        }
+
+    }
+
+}
+
+export default Loader;
