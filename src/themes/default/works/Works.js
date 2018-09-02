ad42a7a782a9cf9c22b031ce87ff16326dd3559c
puterakahfi <puterakahfi@gmail.com>
2018-09-02 19:26:25 +0700

    refactoring Works componnent

---
+++ src/themes/default/works/Works.js
@@ -0,0 +1,41 @@
+import React, {Component, Fragment} from 'react';
+import './Works.css';
+import { Timeline, Event } from "react-timeline-scribble";
+
+class Works extends Component
+{
+
+    render(){
+    
+        return(
+
+            <div className="section" id="experiences">
+            <hr class="section-hr" />
+            <h2>{this.props.works.label}</h2>
+            <hr/>
+           
+    <Timeline>
+    <Fragment>
+    <ListWorks works={this.props.works} />
+      </Fragment> 
+    </Timeline>
+  
+            </div>
+        )
+    }
+}
+
+const ListWorks = (props) =>{
+    const works = props.works.items.map((item) =>
+    <Event interval={item.startDate+" - "+item.endDate} title={item.company} subtitle={item.country}>
+    <h5> {item.position}</h5>
+   <p> {item.summary}</p>
+    <hr/>
+  </Event>)
+
+  return(
+      <span>{works}</span>
+  )
+}
+
+export default Works
